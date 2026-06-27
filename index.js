c0cf4e527a9aecc361b533cfd53786605ec6b37c
anacbarbosa2013-a11y <anacbarbosa2013@gmail.com>
2026-06-27 11:38:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const commands = [
     .setName('rejected')
     .setDescription('O conselho disse não!')
     .toJSON()
+
+  new SlashCommandBuilder()
+    .setName('idk')
+    .setDescription('O conselho não faz a menor ideia.')
+    .toJSON()
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
@@ -51,6 +56,19 @@ url: "https://m.media-amazon.com/images/M/MV5BMjIzMjExMmUtYzU4Yy00MDBkLTg0NDgtNG
           image: {
             url: "https://i.ytimg.com/vi/VsAytZh-AdA/maxresdefault.jpg"
           }
+        }
+
+        if (interaction.commandName === 'idk') {
+    await interaction.reply({
+        content: "🤷 O CONSELHO NÃO FAZ A MENOR IDEIA!!",
+        embeds: [
+            {
+                image: {
+                    url: "https://resizing.flixster.com/8AAomsShvrwMKbzf_RkEWpEL3cc=/375x210/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p11921844_e_h10_ab.jpg"
+                }
+            }
+        ]
+    });
         }
       ]
     });
