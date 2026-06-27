33534cfdc1230cfa6cd494bf59ab433b02868e79
anacbarbosa2013-a11y <anacbarbosa2013@gmail.com>
2026-06-27 11:41:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ const commands = [
   new SlashCommandBuilder()
     .setName('rejected')
     .setDescription('O conselho disse não!')
-    .toJSON()
+    .toJSON(),
 
   new SlashCommandBuilder()
     .setName('idk')
     .setDescription('O conselho não faz a menor ideia.')
     .toJSON()
-];
+]; 
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
@@ -39,40 +39,34 @@ client.on('interactionCreate', async interaction => {
   if (interaction.commandName === 'approved') {
     await interaction.reply({
       content: "💥 O CONSELHO APROVA!! ✔️",
-embeds: [
-  {
-    image: {
-url: "https://m.media-amazon.com/images/M/MV5BMjIzMjExMmUtYzU4Yy00MDBkLTg0NDgtNGQ0NzA0MGNiMTZkXkEyXkFqcGc@._V1_.jpg"    }
-  }
-]
+      embeds: [{
+        image: {
+          url: "https://m.media-amazon.com/images/M/MV5BMjIzMjExMmUtYzU4Yy00MDBkLTg0NDgtNGQ0NzA0MGNiMTZkXkEyXkFqcGc@._V1_.jpg"
+        }
+      }]
     });
   }
 
   if (interaction.commandName === 'rejected') {
     await interaction.reply({
       content: "🚫 O CONSELHO DISSE NÃO!! 💀",
-      embeds: [
-        {
-          image: {
-            url: "https://i.ytimg.com/vi/VsAytZh-AdA/maxresdefault.jpg"
-          }
+      embeds: [{
+        image: {
+          url: "https://i.ytimg.com/vi/VsAytZh-AdA/maxresdefault.jpg"
         }
-
-        if (interaction.commandName === 'idk') {
-    await interaction.reply({
-        content: "🤷 O CONSELHO NÃO FAZ A MENOR IDEIA!!",
-        embeds: [
-            {
-                image: {
-                    url: "https://resizing.flixster.com/8AAomsShvrwMKbzf_RkEWpEL3cc=/375x210/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p11921844_e_h10_ab.jpg"
-                }
-            }
-        ]
-    });
-        }
-      ]
+      }]
     });
   }
-});
+
+  if (interaction.commandName === 'idk') {
+    await interaction.reply({
+      content: "🤷 O CONSELHO NÃO FAZ A MENOR IDEIA!!",
+      embeds: [{
+        image: {
+          url: "https://resizing.flixster.com/8AAomsShvrwMKbzf_RkEWpEL3cc=/375x210/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p11921844_e_h10_ab.jpg"
+        }
+      }]
+    });
+  }
 
 client.login(process.env.TOKEN);
